794f855f013fb755fd5b2571fe33f708ff9308ed
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-10-04 06:45:25 -0400

    :sparkles: feat: add App.js & test files
    -Add App.js so pages actually run properly
    -Add testing files for App.js and landingPage.js
    -Add correct route and pathing to index.js and App.js to get different
    components to show correctly, with the correct URL'show
    
    PKM-2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import LandingPage from "./components/landingPage";
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Link to="/home">Landing Page</Link> 
+      {/* <LandingPage /> */}
+
+    </>
+  );
+};
+
+export default App;
