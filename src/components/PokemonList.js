0599d2ff454c6878344f69fd886c2480bccce87c
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-02-23 15:44:19 -0500

    :lipstick: feat: styles for new buttons
    Create new scss files and adjust components/global styles to implement

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,66 +2,80 @@ import HomePageButton from "./HomePageButton";
 import { HashLink } from "react-router-hash-link";
 import PokemonLoader from "./PokemonLoader";
 import { PokemonListItems } from "./PokemonListItems";
-import { useState, useEffect, useCallback } from "react";
+import { useState } from "react";
 import { GetBatchOfPokemon } from "./fetchPokemon";
 
-
-
 const PokemonList = () => {
-  const [ limitNumber, setLimitNumber ] = useState(10);
-  const [ indexNumber, setIndexNumber ] = useState(19);
-  // const [pokemonList, setPokemonList] = useState([]);
+  const [ limitNumber, setLimitNumber ] = useState(12);
+  const [ indexNumber, setIndexNumber ] = useState(-1);
+  const [ nextButtonClicked, setNextButtonClicked ] = useState(false);
+  const [ previousButtonClicked, setPreviousButtonClicked ] = useState(true);
+
+  const previousPage = (limitNumber, indexNumber) => {
+    const minIndexNumber = -1;
+    if(indexNumber === 143) {
+      setIndexNumber(indexNumber -12);
+      setLimitNumber(12);
+      setPreviousButtonClicked(true); 
+    }
+    if(indexNumber === 11) {
+      setNextButtonClicked(false);
+    }
+    if((indexNumber - 12) >= minIndexNumber) {
+      // console.log(`current index: ${indexNumber} previous button`);
+      setIndexNumber(indexNumber - 12);
+    }
+  }
+
+  const nextPage = (limitNumber, indexNumber) => {
+    setNextButtonClicked(true);
+    const maxIndexNumber = 150;
+    if(indexNumber === 131) {
+      const remainingCharacters = 150 - (indexNumber + 12)
+      setLimitNumber(remainingCharacters);
+      setPreviousButtonClicked(false);
+    }
+    if(indexNumber === 143) {
+      alert("that's all our pokemon 😢");
+    }
+    if((indexNumber + 12) < maxIndexNumber) {
+      // console.log(`current index: ${indexNumber} next button`);
+      setIndexNumber(indexNumber + 12);
+    } 
+  }
   
   const getPokemon = async (limitNumber, indexNumber) => {
     const pokemonResponse = await GetBatchOfPokemon(limitNumber, indexNumber);
     // console.log(pokemonResponse)
     return pokemonResponse;
   }
-  
-  // useEffect(() => {
-  //   (async () => {
-  //     const data = await getPokemon();
-  //     return data;
-  //     // console.log(data)
-  //     // setUpdatedPokemonList(data);
-  //     // console.log(updatedPokemonList)
-  //   })();
-  // }, [])
-  
-  // console.log(getPokemon(limitNumber, indexNumber))
-
-  // const pokemonList = GetBatchOfPokemon(limitNumber, indexNumber);
-  // console.log(pokemonList, 'from parent list component')
-  // useEffect(() => {
-  //   setPokemonList(pokemonList);
-  // }, [pokemonList])
-
-
-  // const pokemonList = [
-  //   {
-  //     name: "bulbasaur",
-  //     id: 1,
-  //     base_experience: 64,
-  //   }, {
-  //     name: "bulbasaur",
-  //     id: 2,
-  //     base_experience: 64,
-  //   }, {
-  //     name: "bulbasaur",
-  //     id: 3,
-  //     base_experience: 64,
-  //   },
-  // ]
-  // ^^^^ that'll display just fine. 
 
   return (
     <>
-      <PokemonLoader limitNumber={limitNumber} indexNumber={indexNumber} 
-        // getPokemonData={getPokemon(limitNumber, indexNumber)}
-      >
+      <section className="pokemon-list">
         <HomePageButton />
-        <PokemonListItems pokemonList={getPokemon(limitNumber, indexNumber)}/>
-      </PokemonLoader>
+        <h1 id="to-top-of-list">Original 151 Pokemon</h1>
+        <ul className="grid-list-all">
+          <PokemonLoader limitNumber={limitNumber} indexNumber={indexNumber} 
+          // getPokemonData={getPokemon(limitNumber, indexNumber)}
+          >
+            <PokemonListItems pokemonList={getPokemon(limitNumber, indexNumber)}/>
+          </PokemonLoader>
+        </ul>
+        <div className="pokemon-list__buttons-link">
+          {nextButtonClicked && 
+            <button type="button" className="pokemon-list__previous-button" onClick={() => previousPage(limitNumber, indexNumber)}>
+            See Last Batch of Pokemon
+            </button>
+          }
+          {previousButtonClicked && 
+            <button type="button" className="pokemon-list__next-button" onClick={() => nextPage(limitNumber, indexNumber)}>
+            See Next Batch of Pokemon
+            </button>
+          }
+          <HashLink to="#to-top-of-list" className="back-to-top">☝️ Back to Top</HashLink>
+        </div>
+      </section>
     </>
   )
 }
