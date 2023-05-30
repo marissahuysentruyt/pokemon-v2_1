bb22ca7c488d1099f43792517240bed79dba3e56
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-05-30 15:13:34 -0400

    WIP :sparkles: feat: BattlePage displays the opponents
    - Displays FeaturedCharacter components for the user & computer
    - Has a layout for when content is loaded and if content isn't loaded

--- src/components/BattlePage.js
+++ src/components/BattlePage.js
@@ -1,17 +1,33 @@
 import { AppContext } from '../AppProvider';
 import { useContext } from 'react';
 import HomePageButton from './HomePageButton';
+import FeaturedCharacter from './FeaturedCharacter';
+import { ComputerPokemon } from './ComputerPokemon';
 
 const BattlePage = () => {
 
-  const { selectedPokemon } = useContext(AppContext);
+  const { selectedPokemon, computerPokemon } = useContext(AppContext);
 
   return selectedPokemon ? (
     <div>
       <HomePageButton />
-      <h1>Defeat your opponent!</h1> 
-      { selectedPokemon.name }
-      <img src={ selectedPokemon.sprites.front_default } alt={selectedPokemon.name} />
+      <h1 className="title">A wild <span className="capitalize">{computerPokemon.name}</span> appeared!</h1> 
+      <h2 className="subtitle">Defeat your opponent!</h2> 
+      <section className="featured-wrapper">
+        <div className="featured__user">
+          <FeaturedCharacter>
+            <h2 className="capitalize">{ selectedPokemon.name }</h2>
+            <img className="featured__image" src={ selectedPokemon.sprites.front_default } alt={selectedPokemon.name} />
+          </FeaturedCharacter>
+        </div>
+
+        <div className="featured__computer">
+          <FeaturedCharacter>
+            <ComputerPokemon />
+          </FeaturedCharacter>
+        </div>
+      </section>
+      
     </div>
   ) : 
     <div>
