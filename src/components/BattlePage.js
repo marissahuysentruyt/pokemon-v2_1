184cfb1039d241da63d5d393507ca0cb0c9ba8dd
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-05-30 15:13:34 -0400

    :sparkles: feat: BattlePage displays the opponents
    - Displays FeaturedCharacter components for the user & computer
    - Has a layout for when content is loaded and if content isn't loaded
    - Link component navigates to new Game page with AppContext variable data

--- src/components/BattlePage.js
+++ src/components/BattlePage.js
@@ -1,23 +1,62 @@
 import { AppContext } from '../AppProvider';
-import { useContext } from 'react';
+import { useContext, useCallback, useEffect } from 'react';
 import HomePageButton from './HomePageButton';
+import FeaturedCharacter from './FeaturedCharacter';
+import { ComputerPokemon } from './ComputerPokemon';
+import { Link } from 'react-router-dom';
+import { GetOnePokemon } from './fetchPokemon';
 
 const BattlePage = () => {
 
   const { selectedPokemon } = useContext(AppContext);
+  const { computerPokemon, setComputerPokemon } = useContext(AppContext);
+
+  const getComputerPokemon = useCallback(async () => {
+      const randomPokemonId = Math.round(Math.random() * 151);
+      const pokemonResponse = await GetOnePokemon(randomPokemonId);
+      setComputerPokemon(pokemonResponse)
+      // console.log(pokemonResponse, 'in function')
+  }, [setComputerPokemon]);
+
+  useEffect(() => {
+    (async () => {
+      await getComputerPokemon();
+    })();
+  }, [getComputerPokemon]);
 
   return selectedPokemon ? (
     <div>
       <HomePageButton />
-      <h1>Defeat your opponent!</h1> 
-      { selectedPokemon.name }
-      <img src={ selectedPokemon.sprites.front_default } alt={selectedPokemon.name} />
+      <h1 className="title">A wild <span className="capitalize">{computerPokemon.name}</span> appeared!</h1> 
+      <h2 className="subtitle">Defeat your opponent!</h2> 
+      <section className="featured-wrapper">
+        <div className="featured__user">
+          <FeaturedCharacter>
+            <h2 className="capitalize">{ selectedPokemon.name }</h2>
+            <img className="featured__image" src={ selectedPokemon.sprites.front_default } alt={selectedPokemon.name} />
+          </FeaturedCharacter>
+        </div>
+
+        <div className='versus'>
+          vs.
+        </div>
+
+        <div className="featured__computer">
+          <FeaturedCharacter>
+            <ComputerPokemon computerPokemon={computerPokemon} />
+          </FeaturedCharacter>
+        </div>
+      </section>
+
+      <Link className="begin-battle-button" to="/game">
+        <div className='capitalize'>{selectedPokemon.name}, you know what to do!</div>
+      </Link>
     </div>
   ) : 
     <div>
       <HomePageButton />
-      <h1>Defeat your opponent!</h1>  
-      <p>Did you select a pokemon yet?</p> 
+      <h1 className="title">Whoops!</h1>  
+      <p className="subtitle">Did you select your pokemon yet?</p> 
     </div>
 }
 
